e781ed77da1cc4dbfc96376708e98c0bd97dacd3
Flo Edelmann <florian-edelmann@online.de>
2018-09-27 15:12:15 +0200

    Support auto programs

--- arduino/dmx-pixel-tubes.ino
+++ arduino/dmx-pixel-tubes.ino
@@ -2,6 +2,13 @@
 #include <ArtnetWifi.h>
 #include <FastLED.h>
 
+// fix VSCode C/C++ IntelliSense
+#ifndef ARDUINO
+#define ARDUINO 100
+#endif
+
+#include <WS2812FX.h>
+
 // Wifi settings
 Basecamp iot{
   Basecamp::SetupModeWifiEncryption::secured,
@@ -13,6 +20,11 @@ Basecamp iot{
 #define LED_PIN 5
 
 CRGB leds[NUM_LEDS];
+
+// WS2812FX settings
+#define WS2812FX_FAKE_LED_PIN 4
+
+WS2812FX ws2812fx = WS2812FX(NUM_LEDS, WS2812FX_FAKE_LED_PIN, NEO_RGB);
 
 // ESP32-to-ESP32 communication settings
 #define RX_PIN 14
@@ -36,7 +48,11 @@ typedef enum {
   receiving
 } PixelTubeState;
 
+// state variables
 PixelTubeState currentState;
+bool isEffectEnabled = false;
+bool isReversed = false;
+bool isHsv = false;
 
 
 
@@ -118,6 +134,14 @@ void showError(uint8_t red, uint8_t green, uint8_t blue) {
 }
 
 
+void ws2812fxShowFunction() {
+  if (isEffectEnabled) {
+    memcpy(leds, ws2812fx.getPixels(), NUM_LEDS * 3);
+    FastLED.show();
+  }
+}
+
+
 ColorTemperature getColorTemperatureFromDmxValue(uint8_t dmxValue) {
   if (dmxValue <= 29) return UncorrectedTemperature; // 0...29: default
   if (dmxValue <= 54) return Candle; // 30...54: 1900K
@@ -129,6 +153,67 @@ ColorTemperature getColorTemperatureFromDmxValue(uint8_t dmxValue) {
   if (dmxValue <= 204) return DirectSunlight; // 180...204: 6000K
   if (dmxValue <= 229) return OvercastSky; // 205...229: 7000K
   return ClearBlueSky; // 230...255: 20000K
+}
+
+
+uint8_t getEffectModeFromDmxValue(uint8_t dmxValue) {
+  if (dmxValue <= 19) return FX_MODE_STATIC;
+  if (dmxValue <= 22) return FX_MODE_BLINK;
+  if (dmxValue <= 25) return FX_MODE_BREATH;
+  if (dmxValue <= 28) return FX_MODE_COLOR_WIPE;
+  if (dmxValue <= 31) return FX_MODE_COLOR_WIPE_INV;
+  if (dmxValue <= 34) return FX_MODE_COLOR_WIPE_REV;
+  if (dmxValue <= 37) return FX_MODE_COLOR_WIPE_REV_INV;
+  if (dmxValue <= 40) return FX_MODE_COLOR_WIPE_RANDOM;
+  if (dmxValue <= 43) return FX_MODE_RANDOM_COLOR;
+  if (dmxValue <= 46) return FX_MODE_SINGLE_DYNAMIC;
+  if (dmxValue <= 49) return FX_MODE_MULTI_DYNAMIC;
+  if (dmxValue <= 52) return FX_MODE_RAINBOW;
+  if (dmxValue <= 55) return FX_MODE_RAINBOW_CYCLE;
+  if (dmxValue <= 58) return FX_MODE_SCAN;
+  if (dmxValue <= 61) return FX_MODE_DUAL_SCAN;
+  if (dmxValue <= 64) return FX_MODE_FADE;
+  if (dmxValue <= 67) return FX_MODE_THEATER_CHASE;
+  if (dmxValue <= 70) return FX_MODE_THEATER_CHASE_RAINBOW;
+  if (dmxValue <= 73) return FX_MODE_RUNNING_LIGHTS;
+  if (dmxValue <= 76) return FX_MODE_TWINKLE;
+  if (dmxValue <= 79) return FX_MODE_TWINKLE_RANDOM;
+  if (dmxValue <= 82) return FX_MODE_TWINKLE_FADE;
+  if (dmxValue <= 85) return FX_MODE_TWINKLE_FADE_RANDOM;
+  if (dmxValue <= 88) return FX_MODE_SPARKLE;
+  if (dmxValue <= 91) return FX_MODE_FLASH_SPARKLE;
+  if (dmxValue <= 94) return FX_MODE_HYPER_SPARKLE;
+  if (dmxValue <= 97) return FX_MODE_STROBE;
+  if (dmxValue <= 100) return FX_MODE_STROBE_RAINBOW;
+  if (dmxValue <= 103) return FX_MODE_MULTI_STROBE;
+  if (dmxValue <= 106) return FX_MODE_BLINK_RAINBOW;
+  if (dmxValue <= 109) return FX_MODE_CHASE_WHITE;
+  if (dmxValue <= 112) return FX_MODE_CHASE_COLOR;
+  if (dmxValue <= 115) return FX_MODE_CHASE_RANDOM;
+  if (dmxValue <= 118) return FX_MODE_CHASE_RAINBOW;
+  if (dmxValue <= 121) return FX_MODE_CHASE_FLASH;
+  if (dmxValue <= 124) return FX_MODE_CHASE_FLASH_RANDOM;
+  if (dmxValue <= 127) return FX_MODE_CHASE_RAINBOW_WHITE;
+  if (dmxValue <= 130) return FX_MODE_CHASE_BLACKOUT;
+  if (dmxValue <= 133) return FX_MODE_CHASE_BLACKOUT_RAINBOW;
+  if (dmxValue <= 136) return FX_MODE_COLOR_SWEEP_RANDOM;
+  if (dmxValue <= 139) return FX_MODE_RUNNING_COLOR;
+  if (dmxValue <= 142) return FX_MODE_RUNNING_RED_BLUE;
+  if (dmxValue <= 145) return FX_MODE_RUNNING_RANDOM;
+  if (dmxValue <= 148) return FX_MODE_LARSON_SCANNER;
+  if (dmxValue <= 151) return FX_MODE_COMET;
+  if (dmxValue <= 154) return FX_MODE_FIREWORKS;
+  if (dmxValue <= 157) return FX_MODE_FIREWORKS_RANDOM;
+  if (dmxValue <= 160) return FX_MODE_MERRY_CHRISTMAS;
+  if (dmxValue <= 163) return FX_MODE_FIRE_FLICKER;
+  if (dmxValue <= 166) return FX_MODE_FIRE_FLICKER_SOFT;
+  if (dmxValue <= 169) return FX_MODE_FIRE_FLICKER_INTENSE;
+  if (dmxValue <= 172) return FX_MODE_CIRCUS_COMBUSTUS;
+  if (dmxValue <= 175) return FX_MODE_HALLOWEEN;
+  if (dmxValue <= 178) return FX_MODE_BICOLOR_CHASE;
+  if (dmxValue <= 181) return FX_MODE_TRICOLOR_CHASE;
+  if (dmxValue <= 184) return FX_MODE_ICU;
+  return FX_MODE_STATIC;
 }
 
 
@@ -151,17 +236,34 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
     FastLED.setTemperature(getColorTemperatureFromDmxValue(data[1]));
   }
 
-  bool isReversed = false;
-  bool isHsv = false;
-
   // DMX channel 3: Pixel Control
   if (length >= 3) {
     isReversed = data[2] >= 128;
     isHsv = (data[2] >= 64 && data[2] <= 127) || (data[2] >= 192);
+    ws2812fx.getSegment()->options = isReversed ? REVERSE : NO_OPTIONS;
   }
 
-  // TODO: implement these:
   // DMX channel 4: Auto Programs
+  if (length >= 4) {
+    if (data[3] >= 10) {
+      isEffectEnabled = true;
+
+      uint8_t mode = getEffectModeFromDmxValue(data[3]);
+      if (ws2812fx.getMode() != mode) {
+        ws2812fx.setMode(mode);
+
+        // TODO: make effect colors adjustable
+        ws2812fx.getSegment()->colors[0] = RED;
+        ws2812fx.getSegment()->colors[1] = GREEN;
+        ws2812fx.getSegment()->colors[2] = BLUE;
+      }
+    }
+    else {
+      isEffectEnabled = false;
+    }
+  }
+
+  // TODO: implement this:
   // DMX channel 5: Program Speed
 
   // DMX channels 6...125: single pixel
@@ -186,7 +288,9 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
     leds[ledIndex].nscale8(data[i]);
   }
 
-  FastLED.show();
+  if (!isEffectEnabled) {
+    FastLED.show();
+  }
 }
 
 
@@ -233,6 +337,11 @@ void setup() {
       // initiated after one minute by the timer, so don't do anything here
     };
 
+    ws2812fx.init();
+    ws2812fx.setCustomShow(ws2812fxShowFunction);
+    ws2812fx.setBrightness(255);
+    ws2812fx.start();
+
     artnet.setArtDmxCallback(onDmxFrame);
     artnet.begin();
   }
@@ -247,6 +356,7 @@ void loop() {
   switch (currentState) {
     case receiving:
       artNetReturnCode = artnet.read();
+      ws2812fx.service();
 
       if (artNetReturnCode != 0) {
         // valid Art-Net packet
